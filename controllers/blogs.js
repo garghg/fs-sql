6dfe6fc3e4a2f0d5bfe8c6e5115c87276da8867b
Haardik <hghaardik@gmail.com>
2026-06-25 10:42:26 -0700

    finished ex-13.14 and ex-13.15

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -8,9 +8,10 @@ router.get('/', async (req, res) => {
     const where = {}
 
     if (req.query.search) {
-        where.title = {
-            [Op.iLike]: `%${req.query.search}%`
-        }
+        where[Op.or] = [
+            { title: { [Op.iLike]: `%${req.query.search}%` } },
+            { author: { [Op.iLike]: `%${req.query.search}%` } }
+        ]
     }
 
     const blogs = await Blog.findAll({
@@ -21,7 +22,8 @@ router.get('/', async (req, res) => {
         attributes: {
             exclude: ['userId']
         },
-        where
+        where,
+        order: [['likes', 'DESC']]
     })
     res.json(blogs)
 })
